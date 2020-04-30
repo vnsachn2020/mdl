8b0f88624cf5e64d1444d3343951393ea72f2ae1
Vanessa Chen <vchen00@stuy.edu>
2020-04-30 14:44:59 -0400

    finish?

--- script.py
+++ script.py
@@ -46,6 +46,83 @@ def run(filename):
                           'blue': [0.2, 0.5, 0.5]}]
     reflect = '.white'
 
+    ARG_COMMANDS = ['line', 'scale', 'move', 'rotate', 'save', 'box', 'sphere', 'torus']
+
     print(symbols)
+
     for command in commands:
+        
         print(command)
+        line = command['op']
+
+        if line in ARG_COMMANDS:
+            args = command['args']
+
+        if line == 'push':
+             copy = stack[-1][:]
+             stack.append(copy)
+
+        elif line == 'pop':
+            stack.pop()
+
+        elif line == 'move':
+            t = make_translate(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+
+        elif line == 'rotate':
+            theta = float(args[1]) * (math.pi / 180)
+            if args[0] == 'x':
+                t = make_rotX(theta)
+            elif args[0] == 'y':
+                t = make_rotY(theta)
+            else:
+                t = make_rotZ(theta)
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+
+        elif line == 'scale':
+            t = make_scale(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+
+        elif line in ['box', 'torus', 'sphere']:
+            polygons = []
+
+            if line == 'box':
+                add_box(polygons,
+                        float(args[0]), float(args[1]), float(args[2]),
+                        float(args[3]), float(args[4]), float(args[5]))
+
+            elif line == 'torus':
+                add_torus(polygons,
+                        float(args[0]), float(args[1]), float(args[2]),
+                        float(args[3]), float(args[4]), step_3d)
+
+            elif line == 'sphere':
+                add_sphere(polygons,
+                        float(args[0]), float(args[1]), float(args[2]),
+                        float(args[3]), step_3d)
+
+            matrix_mult( stack[-1], polygons )
+
+            if command['constants'] in symbols:
+                reflect = command['constants']
+            else:
+                reflect = '.white'
+
+            draw_polygons(polygons, screen, zbuffer, view, ambient, light, symbols, reflect)
+
+        if line == 'line':
+            edges = []
+            add_edge( edges,
+                      float(args[0]), float(args[1]), float(args[2]),
+                      float(args[3]), float(args[4]), float(args[5]) )
+            matrix_mult( stack[-1], edges )
+            draw_lines(edges, screen, zbuffer, color)
+
+        if line == 'display' or line == 'save':
+            if line == 'display':
+                display(screen)
+            else:
+                save_extension(screen, args[0])
